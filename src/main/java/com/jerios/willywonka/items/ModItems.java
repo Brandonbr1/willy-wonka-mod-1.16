dbd1a8f637a6437cc8466f6470d0accd5e365a9d
Java sauce <mohammedbrandon204@gmail.com>
2022-06-11 14:28:42 -0700

    add tools and register the armor

--- src/main/java/com/jerios/willywonka/items/ModItems.java
+++ src/main/java/com/jerios/willywonka/items/ModItems.java
@@ -4,8 +4,8 @@ import com.jerios.willywonka.WillyWonka;
 import com.jerios.willywonka.food.ModFoodBase;
 import com.jerios.willywonka.liquid.ModLiquid;
 import net.minecraft.block.TallGrassBlock;
-import net.minecraft.item.BucketItem;
-import net.minecraft.item.Item;
+import net.minecraft.inventory.EquipmentSlotType;
+import net.minecraft.item.*;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.registries.DeferredRegister;
@@ -25,6 +25,8 @@ public class ModItems {
             () -> new BucketItem(() -> ModLiquid.CHOCLATE_FLUID.get(),
                     new Item.Properties().stacksTo(1).tab(ModItemGroup.WILLYWONKA_GROUP)));
 
+    // food
+
 
     public static final RegistryObject<Item> SUGARGRASS = ITEMS.register( "tall_sugar_grass", () -> new Item(new Item.Properties().tab(ModItemGroup.WILLYWONKA_GROUP).stacksTo(64).food(ModFoodBase.CHOCLATE_BAR) ));
 
@@ -33,10 +35,49 @@ public class ModItems {
 
     public static final RegistryObject<Item> LOLLYPOP3DITEM = ITEMS.register( "lollypop3ditemversion", () -> new Item(new Item.Properties().tab(ModItemGroup.WILLYWONKA_GROUP).stacksTo(64).food(ModFoodBase.CHOCLATE_BAR).food(ModFoodBase.FAST_FOOD)));
 
+    // tools
+
+    public static final RegistryObject<Item> CHOCLATE_SWORD = ITEMS.register("pearl_sword",
+            () -> new SwordItem(ModItemTier.CHOCLATETEIR, 2, 3f,
+                    new Item.Properties()));
+
+    public static final RegistryObject<Item> CHOCLATE_PICKAXE = ITEMS.register("pearl_pickaxe",
+            () -> new PickaxeItem(ModItemTier.CHOCLATETEIR, 0, -2f,
+                    new Item.Properties()));
+
+    public static final RegistryObject<Item> CHOCLATE_SHOVEL = ITEMS.register("pearl_shovel",
+            () -> new ShovelItem(ModItemTier.CHOCLATETEIR, 0, -2f,
+                    new Item.Properties()));
+
+    public static final RegistryObject<Item> CHOCLATE_AXE = ITEMS.register("pearl_axe",
+            () -> new AxeItem(ModItemTier.CHOCLATETEIR, 4, -6f,
+                    new Item.Properties()));
+
+    public static final RegistryObject<Item> CHOCLATE_HOE = ITEMS.register("pearl_hoe",
+            () -> new HoeItem(ModItemTier.CHOCLATETEIR, 0, 0f,
+                    new Item.Properties()));
 
 
+    // armor
 
+    public static final RegistryObject<Item> PEARL_BOOTS = ITEMS.register("choclate_boots",
+            () -> new ArmorItem(ModMaterialArmor.CHOCLATEARMOR, EquipmentSlotType.FEET,
+                    new Item.Properties()));
 
+    public static final RegistryObject<Item> PEARL_CHESTPLATE = ITEMS.register("choclate_chestplate",
+            () -> new ArmorItem(ModMaterialArmor.CHOCLATEARMOR, EquipmentSlotType.CHEST,
+                    new Item.Properties()));
+
+    public static final RegistryObject<Item> PEARL_LEGGINGS = ITEMS.register("choclate_leggings",
+            () -> new ArmorItem(ModMaterialArmor.CHOCLATEARMOR, EquipmentSlotType.LEGS,
+                    new Item.Properties()));
+
+    public static final RegistryObject<Item> PEARL_HELMET = ITEMS.register("choclate_helmet",
+            () -> new ArmorItem(ModMaterialArmor.CHOCLATEARMOR, EquipmentSlotType.HEAD,
+                    new Item.Properties()));
+
+    public static final RegistryObject<Item> CHOCLATE_BOW = ITEMS.register("choclate_bow",
+            () -> new BowItem(new Item.Properties().stacksTo(1)));
 
 
     public static void register(IEventBus eventBus) {
--- src/main/java/com/jerios/willywonka/items/ModMaterialArmor.java
+++ src/main/java/com/jerios/willywonka/items/ModMaterialArmor.java
@@ -11,7 +11,7 @@ import net.minecraftforge.api.distmarker.OnlyIn;
 
 public enum ModMaterialArmor implements IArmorMaterial{
 
-    CHOCLATEARMOR("choclate_armor", 7, new int[] {3, 6, 8,3}, 12, SoundEvents.ARMOR_EQUIP_DIAMOND, 3.0f, 0.0f, (null)  );
+    CHOCLATEARMOR("choclate_armor", 7, new int[] {2, 5, 6, 2}, 12, SoundEvents.ARMOR_EQUIP_DIAMOND, 3.0f, 0.0f, (null));
 
 
 
