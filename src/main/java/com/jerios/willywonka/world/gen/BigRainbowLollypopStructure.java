83a2eeeda615d89673d89dafe611aea66d6ef0ab
Java sauce <mohammedbrandon204@gmail.com>
2022-06-12 12:56:33 -0700

    tree gen
    factory gen(soon)
    large gheese entity lays egg(soon)

---
+++ src/main/java/com/jerios/willywonka/world/gen/BigRainbowLollypopStructure.java
@@ -0,0 +1,11 @@
+package com.jerios.willywonka.world.gen;
+
+import net.minecraftforge.event.world.BiomeLoadingEvent;
+
+public class BigRainbowLollypopStructure {
+
+    public static void GenerateLollypops(final BiomeLoadingEvent event) {
+        //LollypopTress = lollypopTree = new LollypopTress();
+
+    }
+}
