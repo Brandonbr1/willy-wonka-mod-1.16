d1af2f8630526d4a73a0dc39db89b5f8804b2160
Java sauce <mohammedbrandon204@gmail.com>
2022-06-11 17:01:45 -0700

    add more stuff to en_us.json

--- src/main/java/com/jerios/willywonka/enities/AgressiveOompaLoompaEntity.java
+++ src/main/java/com/jerios/willywonka/enities/AgressiveOompaLoompaEntity.java
@@ -1,4 +1,15 @@
 package com.jerios.willywonka.enities;
 
-public class AgressiveOompaLoompaEntity extends {
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.monster.ZombieEntity;
+import net.minecraft.world.World;
+
+public class AgressiveOompaLoompaEntity extends ZombieEntity {
+    public AgressiveOompaLoompaEntity(EntityType<? extends ZombieEntity> p_i48549_1_, World p_i48549_2_) {
+        super(p_i48549_1_, p_i48549_2_);
+    }
+
+    public AgressiveOompaLoompaEntity(World p_i1745_1_) {
+        super(p_i1745_1_);
+    }
 }
